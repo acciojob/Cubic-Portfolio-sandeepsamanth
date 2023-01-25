08e5f299b1dba5d8c0ab5e8121addc722357fa86
sandeepsamanth <98205088+sandeepsamanth@users.noreply.github.com>
2023-01-25 19:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+const cube = document.getElementById("cube");
+let angle = 0;
+
+setInterval(() => {
+  angle += 1;
+  cube.style.transform = `rotateY(${angle}deg) rotateX(${angle}deg)`;
+}, 30);
